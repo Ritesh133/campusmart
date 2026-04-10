122693d4f359514e8320922869210e5320e5351c
ritesh133 <ritesh22407@iiitd.ac.in>
2026-04-11 02:41:34 +0530

    Fix static files loading via WhiteNoise

--- campusmart/settings.py
+++ campusmart/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Enable WhiteNoise compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media files (user uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
